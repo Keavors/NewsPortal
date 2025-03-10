dd81288b2a1beed8fe3b404aa767a42e9ddfb141
kvrsss <petr.famusov.01@mail.ru>
2025-03-11 00:19:17 +0300

    Исправлены файлы: news_detail.html, news_list.html, views.py. Добавлен порядок вывода новости: заголовок, дата публикации в формате день.месяц.год, затем первые 20 слов текста статьи.

--- news/views.py
+++ news/views.py
@@ -9,7 +9,11 @@ class NewsList(ListView):
     model = Post
     template_name = 'news/news_list.html'
     context_object_name = 'news_list'
-    queryset = Post.objects.filter(post_type='news').order_by('-created_at')
+    ordering = ['-created_at']  # Сортировка от новых к старым
+    paginate_by = 10  # Опционально: пагинация
+
+    def get_queryset(self):
+        return Post.objects.filter(post_type='news').order_by('-created_at')
 
 class NewsDetail(DetailView):
     model = Post
